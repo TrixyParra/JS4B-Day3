8b4e64ec7464592296aad89b74e051f9a35f58fb
TrixyParra <trixy.parra@gmail.com>
2022-05-24 05:09:05 -0700

    Part 3 of Lecture Notes. Alert, Confirm, Prompt. Math Object. Date Project - running and upadting LIVE.

--- js/main.js
+++ js/main.js
@@ -26,9 +26,13 @@ console.log('this will run as well');
 true;  // 1, yes 
 false;  // 0, no 
 
+
 // Complex Data Types (compound data types) (organize data) 
 
+// array 
 array = [];  // array is a list of values; 
+
+// object 
 object = {};  // object is a list of key value pairs; (dictionary); 
 
 {
@@ -41,6 +45,8 @@ whatIsThis.innerHTML = "this is a string";  // this is a string  - shows it on H
 // an element is an object 
 console.dir(whatIsThis); 
 
+//  =  is an operator
+
 let score = 0; 
 console.log(score + 10);  // 10 
 console.log(score);  // 0 
@@ -50,6 +56,7 @@ score += 1;
 score += 1;  // this will add both line 49 and 50 until logged on the console again 
 console.log(score);  // 12 
 
+// + += add  -> += add and assign 
 // - -= subtract 
 // * *= multiply 
 // / /= divide 
@@ -60,7 +67,7 @@ console.log(7 % 5);  // 2
 console.log(12 % 5);  // 2  // loop? 
 
 
-// Template Literals ${____} 
+// Template Literals ${____}  - where ____ is a variable 
 const noun = 'friend'; 
 const catchphrase = `I am a ${noun}.`;  // I am a friend.  // nested 
 console.log(catchphrase);  // to console a variable - no type of quotations in paranethesis 
@@ -148,7 +155,7 @@ const answerEx = `.${value}`;
 document.querySelector('body').innerHTML = answerEx;  // .9 
 console.log(answerEx);  // .9 
 
-// .padStart(_, '_')  - two digits instead of single digit answer 
+// .padStart(_, '_')  - two digits instead of single digit answer - 0 added if not a double digit number 
 const valueTwo = '5'; 
 const answr = `.${valueTwo.padStart(2, '0')}`; 
 document.querySelector('body').innerHTML = `<h1>${answr}</h1>`;  // .05  - in h1 element 
@@ -165,4 +172,120 @@ const valueFour = '10';
 const answerFour = `.${valueFour.replace('0', 'x')}`; 
 document.querySelector('body').innerHTML = answerFour;  // .1x  - replaces 0 with x 
 console.log(answerFour);  // .1x 
+// more .replace('' '')
+console.log('101010101'.replace('0', 'x'));  // 1x1010101  - only the first 0 turns to x 
+console.log('101010101'.replaceAll('0', 'x'));  // 1x1x1x1x1  - all 0's turn to x 
 
+// .split('') 
+const valueFive = '  10  '; 
+const answerFive = valueFive.split('');  // no longer in back ticks, template literal and no decimal before value 
+document.querySelector('body').innerHTML = answerFive;  // _, ,1,0, ,_   - splits each character including spaces 
+console.log(answerFive);  // _, ,1,0, ,_  
+
+
+// Alert, Confirm, Prompt 
+
+/*
+// alert(''); 
+alert('Welcome to my page!');  // okay - undefined on console
+
+// confirm(''); 
+confirm('Are you sure?');  // okay or cancel - true or false on console 
+
+// prompt(''); 
+prompt('What is your name?');  // insert name then okay or cancel - '___' on console 
+
+const myAlert = alert('Welcome to my page!'); 
+const myConfirm = confirm('Are you sure'); 
+const myPrompt = prompt('What is your name'); 
+console.log(myAlert, myConfirm, myPrompt);  // undefined, true, 'Trixy' 
+
+newAnswer = myPrompt; 
+document.querySelector('body').innerHTML = `<h1>Hello, ${newAnswer}</h1>`;  // Hello, Trixy 
+console.log(newAnswer);  // Trixy  - puts the input added on answer to myPrompt prompt 
+*/
+
+
+// Math Object 
+
+//Math 
+console.log(Math);  // Math {abs: ƒ, acos: ƒ, acosh: ƒ, asin: ƒ, asinh: ƒ, …} 
+
+console.log(Math.abs(-5));  // 5 
+console.log(Math.acos(0.5));  // 1.0471975511965979 
+
+// Math.max(__, __, __,) 
+console.log(Math.max(40, 22, 2, 55, 20, 8, 42));  // 55  - highest number 
+
+// Math.min(__, __, __,) 
+console.log(Math.min(40, 22, 2, 55, 20, 8, 42));  // 2  - lowest number 
+
+// Math.random() 
+console.log(Math.random());  // 0.48478748370850044  - random number - will always change on each console check 
+console.log(Math.random());  // 0.38594500591799163  - another random number - will always change on each console check 
+console.log(Math.random());  // 0.9094579854526734  - other random number - will always change on each console check 
+
+// Math.floor(Math.random()) 
+console.log(Math.floor(Math.random()));  // 0  - always 0 ? 
+
+// Math.flor(Math.random() * 5) - any number between 0 and 5 
+console.log(Math.floor(Math.random() * 5));  // 0  - random number between 0 and 5 - always changes on each console check 
+console.log(Math.floor(Math.random() * 5));  // 3  - random number between 1 and 5 - always changes on each console check 
+console.log(Math.floor(Math.random() * 5));  // 1  - random number between 1 and 5 - always changes on each console check 
+
+// Math.ceil(Math.random())
+console.log(Math.ceil(Math.random()));  // 1  - always 1 ? 
+
+// Math.ceil(Math.random() *5) - any number between 1 and 5 
+console.log(Math.ceil(Math.random() * 5));  // 3  - random number between 1 and 5 - always changes on each console check 
+console.log(Math.ceil(Math.random() * 5));  // 1  - random number between 1 and 5 - always changes on each console check 
+console.log(Math.ceil(Math.random() * 5));  // 5  - random number between 1 and 5 - always changes on each console check 
+
+
+// Date Object 
+
+// Date() 
+console.log(Date());  // Tue May 24 2022 03:46:21 GMT-0700 (Pacific Daylight Time) - changes on each console check 
+dateAnswer = Date(); 
+document.querySelector('body').innerHTML = dateAnswer;  // Tue May 24 2022 03:46:21 GMT-0700 (Pacific Daylight Time) - changes on each console check 
+
+document.querySelector('body').innerHTML = `This is the date information`; // This is the date information  - on html 
+
+dateAnswerTwo = new Date(); 
+document.querySelector('body').innerHTML = `<h1>This is the date information</h1>
+<p>Month: ${dateAnswerTwo.getMonth()}</p>
+`;  // Month: 4  - month 4 because January is 0 - months begins at 0 and so on 
+console.log(dateAnswerTwo);  // Tue May 24 2022 04:13:43 GMT-0700 (Pacific Daylight Time) 
+
+console.dir(new Date());  // Tue May 24 2022 04:14:33 GMT-0700 (Pacific Daylight Time) 
+dateAnswerThree = new Date(); 
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; 
+document.querySelector('body').innerHTML = 
+`
+<h1>This is the date information</h1>
+<p>Month: ${months[dateAnswerThree.getMonth()]}</p>
+<p>Day: ${dateAnswerThree.getDate()}</p>
+<p>Year: ${dateAnswerThree.getFullYear()}</p>
+<p>Hours: ${dateAnswerThree.getHours() % 12 /* added module */}</p>
+<p>Minutes: ${dateAnswerThree.getMinutes()}</p>
+<p>Seconds: ${dateAnswerThree.getSeconds()}</p>
+`;  // Month: May   Day: 24   Year: 2022   Hours: 4   Minutes:31   Seconds: 52 
+
+// keeps it running updating LIVE  
+setInterval(() => {
+    dateAnswerThree = new Date();
+    document.querySelector('body').innerHTML = 
+    `
+    <h1>This is the date information</h1>
+    <p>Month: ${months[dateAnswerThree.getMonth()]}</p>
+    <p>Day: ${dateAnswerThree.getDate()}</p>
+    <p>Year: ${dateAnswerThree.getFullYear()}</p>
+    <p>Hours: ${dateAnswerThree.getHours() % 12}</p>
+    <p>Minutes: ${dateAnswerThree.getMinutes()}</p>
+    <p>Seconds: ${dateAnswerThree.getSeconds()}</p>
+    <p>Time: ${dateAnswerThree.getHours() > 12 ? 'PM' : 'AM'}</p>
+    <p>${ String(dateAnswerThree.getHours() % 12).padStart(2, 0)} : ${dateAnswerThree.getMinutes().toString().padStart(2, '0')} : ${dateAnswerThree.getSeconds().toString().padStart(2, '0')}</p>
+    `;
+    console.log('running..')
+}, 1000); 
+// Month: May   Day: 24   Year: 2022   Hours: 4   Minutes:31   Seconds: 52   Time: AM   04:52:20   
